f0cdfc3ec007c63fefafe12de454067826187176
duminoi <dminso1koaiso2@gmail.com>
2024-06-18 18:02:30 +0700

    update

--- js/couponAdd.js
+++ js/couponAdd.js
@@ -1,24 +1,12 @@
-// var inputDates = document.querySelectorAll(".timeUse .input-wrapper input");
-// var iconDates = document.querySelectorAll(".timeUse .input-wrapper img");
-// // var starDay = document.querySelector("#startDay");
-// console.log(inputDates);
-// console.log(iconDates);
-
-// iconDates.forEach(function (icon, index) {
-//   icon.addEventListener("click", function () {
-//     if (!inputDates[index].open) {
-//       inputDates[index].focus();
-//       inputDates[index].open();
-//     }
-//   });
-// });
-
 var escBtn = document.querySelector(".escBtn");
 var addBtn = document.querySelector("button.add");
 escBtn.addEventListener("click", function () {
   window.location.href = "./../couponList.html";
 });
-
+var form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
 //toastBox
 const toastBox = document.querySelector(".toastBox");
 var successMsg = `<i class="fa-solid fa-circle-check"></i> Thêm thành công`;
@@ -30,7 +18,7 @@ var showToast = function (msg) {
   toastBox.appendChild(toast);
   setTimeout(function () {
     toastBox.removeChild(toast);
-  }, 2000);
+  }, 5000);
 };
 
 //kiểm tra dữ liệu khi bấm nút sửa
@@ -49,3 +37,87 @@ addBtn.addEventListener("click", function (e) {
     showToast(successMsg);
   }
 });
+
+//Thêm mới dữ liệu
+var productsAPI = "http://localhost:3000/products";
+function start() {
+  getProducts(renderProducts);
+  handleCreateProducts();
+}
+start();
+function getProducts(callback) {
+  fetch(productsAPI)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(callback);
+}
+
+function createProducts(data, callback) {
+  var option = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+  fetch(productsAPI, option)
+    .then(function (response) {
+      response.json();
+    })
+    .then(callback);
+}
+
+function renderProducts(products) {
+  var interface = products.map(function (product, index) {
+    return `
+    <tr class="product-${index}">
+    <th scope="row"><input type="radio" name="option" /></th>
+    <td>${product.ma}</td>
+    <td>${product.thoiGian}</td>
+    <td>${product.giaTriGiam}</td>
+    <td>${product.luotSuDung}</td>
+    <td>${product.minUse}đ</td>
+    <td>${product.trangThai}</td>
+    <td>
+      <div class="action">
+        <i class="fa-solid fa-eye"></i>
+        <i class="fa-solid fa-pencil"></i>
+        <i class="fa-solid fa-trash product-${index}"></i>
+      </div>
+    </td>
+  </tr>
+    `;
+  });
+  localStorage.setItem("couponList", JSON.stringify(interface)); //Lưu tại interface trên localStrorage qua couponList
+}
+
+function handleCreateProducts() {
+  addBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    var name = document.querySelector(`input[name="name"]`).value;
+    var startDay = document.querySelector(`input[name="startDay"]`).value;
+    var endDay = document.querySelector(`input[name="endDay"]`).value;
+    var discountValue = document.querySelector(
+      `input[name="discountValue"]`
+    ).value;
+    var limitUse = document.querySelector(`input[name="limitUse"]`).value;
+    var limitValue = document.querySelector(`input[name="limitValue"]`).value;
+    var status = document.querySelector(`select[name="status"]`);
+    var selectedIndex = status.selectedIndex;
+    var selectedOption = status.options[selectedIndex].value;
+
+    var formData = {
+      ma: name,
+      thoiGian: `${startDay}-${endDay}`,
+      giaTriGiam: discountValue,
+      luotSuDung: limitUse,
+      minUse: limitValue,
+      trangThai: selectedOption,
+    };
+
+    createProducts(formData, function () {
+      window.location.href = "./../couponList.html";
+    });
+  });
+}
--- js/couponList.js
+++ js/couponList.js
@@ -4,13 +4,103 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "./../couponAdd.html";
   });
 
-  var updateIcons = document.querySelectorAll(".action .fa-pencil");
+  // hiển thị dữ liệu từ trường nhập vào bảng
+  // var productsAPI = "http://localhost:3000/products";
+  // function start() {
+  //   getProducts(renderProducts);
+  // }
+  // start();
+  // function getProducts(callback) {
+  //   fetch(productsAPI)
+  //     .then(function (response) {
+  //       return response.json();
+  //     })
+  //     .then(callback);
+  // }
 
+  // function renderProducts(products) {
+  //   var tBody = document.querySelector(".table-wrapper tbody");
+  //   var interface = products.map(function (product) {
+  //     return `
+  //     <tr>
+  //     <th scope="row"><input type="radio" name="option" /></th>
+  //     <td>${product.ma}</td>
+  //     <td>${product.thoiGian}</td>
+  //     <td>${product.giaTriGiam}</td>
+  //     <td>${product.luotSuDung}</td>
+  //     <td>${product.minUse}đ</td>
+  //     <td>${product.trangThai}</td>
+  //     <td>
+  //       <div class="action">
+  //         <i class="fa-solid fa-eye"></i>
+  //         <i class="fa-solid fa-pencil"></i>
+  //         <i class="fa-solid fa-trash"></i>
+  //       </div>
+  //     </td>
+  //   </tr>
+  //     `;
+  //   });
+  //   tBody.innerHTML = interface.join("");
+  //   console.log(interface);
+  //   //bấm vào icon bút --> sang giao diện sửa
+  //   var updateIcons = document.querySelectorAll(".action .fa-pencil");
+  //   updateIcons.forEach(function (updateicon) {
+  //     updateicon.addEventListener("click", function () {
+  //       window.location.href = "./../couponUpdate.html";
+  //     });
+  //   });
+  // }
+  var productsAPI = "http://localhost:3000/products";
+  function start() {
+    getProducts(renderProducts);
+  }
+  start();
+  function getProducts(callback) {
+    fetch(productsAPI)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(callback);
+  }
+  function renderProducts(products) {
+    var interface = products.map(function (product, index) {
+      return `
+      <tr class="product-${index}">
+      <th scope="row"><input type="radio" name="option" /></th>
+      <td>${product.ma}</td>
+      <td>${product.thoiGian}</td>
+      <td>${product.giaTriGiam}</td>
+      <td>${product.luotSuDung}</td>
+      <td>${product.minUse}đ</td>
+      <td>${product.trangThai}</td>
+      <td>
+        <div class="action">
+          <i class="fa-solid fa-eye"></i>
+          <i class="fa-solid fa-pencil"></i>
+          <i class="fa-solid fa-trash product-${index}"></i>
+        </div>
+      </td>
+    </tr>
+      `;
+    });
+    localStorage.setItem("couponList", JSON.stringify(interface)); //Lưu tại interface trên localStrorage qua couponList
+  }
+  const tbody = document.querySelector("tbody");
+  const saveCounpons = localStorage.getItem("couponList"); // lấy ra saveCoupons trên localStrorage thông qua couponList
+  tbody.innerHTML = JSON.parse(saveCounpons).join("");
+  var trashicons = document.querySelectorAll(`.action i.fa-trash`);
+  trashicons.forEach(function (icon, i) {
+    icon.addEventListener("click", function () {
+      var tr = document.querySelector(`tr[class=product-${i}]`);
+      tbody.removeChild(tr);
+    });
+  });
+
+  //  bấm vào icon bút --> sang giao diện sửa
+  var updateIcons = document.querySelectorAll(".action .fa-pencil");
   updateIcons.forEach(function (updateicon) {
     updateicon.addEventListener("click", function () {
       window.location.href = "./../couponUpdate.html";
     });
   });
-  // hiển thị dữ liệu từ trường nhập vào bảng
-  var tBody = document.querySelector("tbody");
 });

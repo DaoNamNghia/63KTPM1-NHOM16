dcd3f4e6a92f826ae5d0d59c56a8070b8f82b027
duminoi <dminso1koaiso2@gmail.com>
2024-06-21 03:09:17 +0700

    u

--- js/couponList.js
+++ js/couponList.js
@@ -7,53 +7,6 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("chuyển hướng");
   });
 
-  // hiển thị dữ liệu từ trường nhập vào bảng
-  // var productsAPI = "http://localhost:3000/products";
-  // function start() {
-  //   getProducts(renderProducts);
-  // }
-  // start();
-  // function getProducts(callback) {
-  //   fetch(productsAPI)
-  //     .then(function (response) {
-  //       return response.json();
-  //     })
-  //     .then(callback);
-  // }
-
-  // function renderProducts(products) {
-  //   var tBody = document.querySelector(".table-wrapper tbody");
-  //   var interface = products.map(function (product) {
-  //     return `
-  //     <tr>
-  //     <th scope="row"><input type="radio" name="option" /></th>
-  //     <td>${product.ma}</td>
-  //     <td>${product.thoiGian}</td>
-  //     <td>${product.giaTriGiam}</td>
-  //     <td>${product.luotSuDung}</td>
-  //     <td>${product.minUse}đ</td>
-  //     <td>${product.trangThai}</td>
-  //     <td>
-  //       <div class="action">
-  //         <i class="fa-solid fa-eye"></i>
-  //         <i class="fa-solid fa-pencil"></i>
-  //         <i class="fa-solid fa-trash"></i>
-  //       </div>
-  //     </td>
-  //   </tr>
-  //     `;
-  //   });
-  //   tBody.innerHTML = interface.join("");
-  //   console.log(interface);
-  //   //bấm vào icon bút --> sang giao diện sửa
-  //   var updateIcons = document.querySelectorAll(".action .fa-pencil");
-  //   updateIcons.forEach(function (updateicon) {
-  //     updateicon.addEventListener("click", function () {
-  //       window.location.href = "./../couponUpdate.html";
-  //     });
-  //   });
-  // }
-
   // gọi API
   var productsAPI = "http://localhost:3000/products";
   function start() {
